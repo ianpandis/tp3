aa3f27fbf84e14d65c4e0bf713e0a121d22aa941
mateo ferrari coronel <mateo.ferrari97@gmail.com>
2019-07-08 19:38:58 -0300

    -

--- AlgoPoli.py
+++ AlgoPoli.py
@@ -7,7 +7,8 @@ def main():
         print("Error: Cantidad de parametros incorrectos. Intente nuevamente.") 
         return
     archivo = cargar_grafo(sys.argv[1])
-    divulgar_ciclo(archivo, 63, 1)
+    cfc(archivo)
+    #divulgar_ciclo(archivo, 63, 1) #falta este tambien
     #divulgar(archivo, 1, 7)
     #min_seguimientos(archivo, 1, 7)
     
@@ -53,4 +54,40 @@ def divulgar(grafo, delincuente, n):
 def divulgar_ciclo(grafo, delincuente, n):
     resultado = grafo.obtener_ciclo_bfs()
     print(resultado)
+
+def dfs_cfc(vertices, vertice, visitados, orden, lista1, lista2, cfcs, en_cfs):
+    visitados.add(vertice)
+    lista1.append(vertice)
+    lista2.append(vertice)
+    for adyacente in vertices[vertice]:
+        if adyacente not in visitados:
+            orden[adyacente] = orden[vertice] + 1
+            dfs_cfc(vertices, adyacente, visitados, orden, lista1, lista2, cfcs, en_cfs)
+        elif adyacente not in en_cfs:
+            while orden[lista1[-1]] > orden[adyacente]:
+                lista1.pop()
+
+    if lista1.ver_tope() == vertice:
+        lista1.pop()
+        aux = None
+        nueva_cfc = []
+        while aux != vertice:
+            aux = lista2.pop()
+            en_cfs.add(aux)
+            nueva_cfc.append(aux)
+        cfcs.add(nueva_cfc)
+
+def cfc(grafo):
+	visitados = set()
+	orden = {}
+	lista1 = []
+	lista2 = []
+	cfcs = []
+	en_cfs = set()
+	for vertice in grafo.vertices:
+		if vertice not in visitados:
+			orden[vertice] = 0
+			dfs_cfc(grafo.vertices, vertice, visitados, orden, lista1, lista2, cfcs, en_cfs)
+	return cfcs
+
 main()
